03c6baa4c31b325578c07b587219f482f5bd0d99
aptx4869tao <t2062558563@gmail.com>
2022-04-23 17:11:51 +0800

    router

---
+++ 12_learn_router/src/App.js
@@ -0,0 +1,25 @@
+import React, { PureComponent } from 'react'
+import { BrowserRouter,Link,Route, Routes } from 'react-router-dom'
+import About from './page/about'
+import Home from './page/home'
+import Profile from './page/profile'
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+      <BrowserRouter>
+        <Link to='/'>首页</Link>
+        <Link to='/about'>关于</Link>
+        <Link to='/profile'>我的</Link>
+        <Routes>
+            <Route exact path='/' element={<Home/>}/>
+            <Route exact path='/about' element={<About/>}/>
+            <Route exact path='/profile' element={<Profile/>}/>
+        </Routes>
+      </BrowserRouter>
+          {/* <About/>
+          <Home/> */}
+      </div>
+    )
+  }
+}
